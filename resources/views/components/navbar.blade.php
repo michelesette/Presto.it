6d622f57f866ec90a651bdfe7f27efdc0f06233f
Michele Sette <settemichele27@gmail.com>
2024-05-09 18:01:49 +0200

    aggiutna la possibilità di fare il logout e aggiunto il saluto all'utente che si regsitra

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -11,7 +11,21 @@
           <a class="nav-link" href="{{route('login')}}">Accedi</a>
           <a class="nav-link" href="{{route('register')}}">Registrati</a>
           @endguest
-          <a class="nav-link" href="{{route('logout')}}">Logout</a>
+          @auth
+          <li class="nav-item">
+           <a class="nav-link" href="#">Ciao {{Auth::user()->name}}</a>
+         </li>
+           <li class="nav-item">
+             <form          
+             action="{{route('logout')}}" 
+             method="POST">
+             @csrf
+             <button
+             class="nav-link"
+             type="submit">Logout</button>      
+             </form>
+           </li>
+           @endauth
         </div>
       </div>
     </div>
