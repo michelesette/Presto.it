c6db404f8c180495b721f7a6d57d6a793e66b31f
Karim Ben Abid <veas_97@hotmail.it>
2024-05-09 17:11:04 +0200

    aggiunte le rotte login + register + logout

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,9 +7,11 @@
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
           <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
+          @guest
           <a class="nav-link" href="{{route('login')}}">Accedi</a>
           <a class="nav-link" href="{{route('register')}}">Registrati</a>
-          <a class="nav-link" href="#">Logout</a>
+          @endguest
+          <a class="nav-link" href="{{route('logout')}}">Logout</a>
         </div>
       </div>
     </div>
