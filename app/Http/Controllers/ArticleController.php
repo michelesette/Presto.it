7bd3efee0551ed91f91ea23bd52792c3bdb0e1fc
Marianna Conticelli <mariannaconticelli2@gmail.com>
2024-05-13 13:03:42 +0200

    aggiunto funzione ordinamento dal più recente al più vecchio degli articoli nella index

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -14,7 +14,7 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        $articles = Article::all();
+        $articles = Article::orderBy('created_at', 'desc')->get();
         return view('article.index', compact('articles'));
     }
 
