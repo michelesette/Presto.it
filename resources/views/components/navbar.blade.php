792be89a66a34dfcebd00156f2a06dde30752e57
Karim Ben Abid <veas_97@hotmail.it>
2024-05-13 19:16:45 +0200

    modificata la navbar e sistemate la struttura

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -43,7 +43,7 @@
                   @if (Auth::user()->is_admin)
                   <li><a class="dropdown-item nav-link  my-3" href="{{route('dashboard')}}"><i class="fa-solid fa-user-tie"></i> Dashboard Admin</a></li>
                   @elseif(!Auth::user()->is_lessor)
-                  <li><a class="dropdown-item nav-link  my-3" href="#"><i class="fa-solid fa-hotel"></i> Diventa Locatore</a></li>
+                  
                   @endif
                   <li>
                     <a class="dropdown-item nav-link  my-3" href="{{route('article.create')}}"><i class="bi bi-plus-lg"></i>Articolo</a>
