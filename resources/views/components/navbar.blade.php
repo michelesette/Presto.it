8287056bdf00610ec6f7a74f6d97d49460e86b50
Michele Sette <Michele@MacBookiMichele.station>
2024-05-14 18:20:41 +0200

    aggiunta nella navbar sezione lavora con noi

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -11,6 +11,9 @@
         <li class="nav-item px-3 ">
           <a class="nav-link" href="{{route('article.index')}}"><i class="fa-solid fa-newspaper"></i> Articoli</a>
         </li>
+        <li class="nav-item px-3 ">
+          <a class="nav-link" href="{{route('careers')}}"><i class="bi bi-pc-display"></i>Lavora con noi</a>
+        </li>
         @guest
         <div>
           <ul class="navbar-nav">
