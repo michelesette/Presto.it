7dac2780c8ecbbb228b92c631a0f6381f8c10bec
Vins-17 <vincenzocioffi45@gmail.com>
2024-05-15 18:01:05 +0200

    dasboard admin funzionante con richieste di ruoli

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -14,4 +14,25 @@ class AdminController extends Controller
 
         return view('admin.dashboard',compact('adminRequest','revisorRequest','writerRequest'));
     }
+
+    public function setAdmin(User $user){
+          $user->is_admin = true;
+          $user->save();
+          
+          return redirect(route('admin.dashboard'))->with('message', 'Hai corettamente reso amministratore 1\'utente scelto');
+    }
+
+    public function setRevisor(User $user){
+        $user->is_revisor = true;
+        $user->save();
+        
+        return redirect(route('admin.dashboard'))->with('message', 'Hai corettamente reso revisore 1\'utente scelto');
+  }
+
+  public function setWriter(User $user){
+    $user->is_writer = true;
+    $user->save();
+    
+    return redirect(route('admin.dashboard'))->with('message', 'Hai corettamente reso redattore 1\'utente scelto');
+}
 }
