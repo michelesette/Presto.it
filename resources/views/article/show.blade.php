be4ee81fe2dcd162c43d9eb00780ed47a9810df2
Michele Sette <Michele@MacBookiMichele.station>
2024-05-15 14:54:30 +0200

    pagina dettaglio articoli

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -1,6 +1,6 @@
 <x-layout title="Articolo - {{ $article->title }}">
     <header class="header pt-5 articleCont">
-        <div class="container-fluid mt-5 p-5 rounded-3">
+        <div class="container-fluid mt-5 p-5 mb-5 rounded-3">
             <div class="row justify-content-center align-content-center h-100">
                 <div class="col-12 text-bg-light  d-flex justify-content-center align-items-center">
                     <h1 class="text-center">{{ $article->title }}</h1>
@@ -15,49 +15,36 @@
     {{-- Snippet per verificare errori --}}
     <x-display-errors />
 
-    <div class="container mt-5 newArt">
-        <div class="row mt-5 justify-content-center my-5">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 d-flex justify-content-center">
-                        <div class=" container card my-5 py-5" style="max-width: 540px;">
-                            <div class="row d-flex justify-content-center">
-                                <div class="col-md-4">
-                                    <img src="{{ Storage::url($article->img) }}" class="img-fluid rounded-start"
-                                    alt="...">
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="card-body">
-                                        <h5 class="card-title">{{ $article->title }}</h5>
-                                        <h5 class="card-title p-2">{{ $article->subtitle }}</h5>
-                                        <p class="card-text">{{ $article->body }}</p>
-                                        @if ($article->category)
-                                        <div>
-                                            <span
-                                            class="badge text-bg-primary">#{{ $article->category->name }}</span>
-                                        </div>
-                                        @endif
-
-                                        @auth
-                                        <a href="{{ route('article.edit', compact('article')) }}"
-                                        class="btn mybtn mt-3 ">Modifica</a>
-                                        <form action="{{ route('article.destroy', compact('article')) }}"
-                                        method="POST">
-                                        @method('DELETE')
-                                        @csrf
-                                        <button type="submit" class="btn btn-danger  mt-3 ">Elimina</button>
-                                    </form>
-                                    @endauth
-                                </div>
-                            </div>
-                        </div>
+    <div class="container d-flex mt-5 ">
+        <div class="row bg-primary ">
+            <div class="col-12-col-md-6">
+                <div class="col-md-4">
+                    <img src="{{ Storage::url($article->img) }}" class="img-fluid rounded-3" alt="...">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12-col-md-6">
+                    <h5 class="card-title">{{ $article->title }}</h5>
+                    <h5 class="card-title p-2">{{ $article->subtitle }}</h5>
+                    <p class="card-text">{{ $article->body }}</p>
+                    @if ($article->category)
+                    <div>
+                        <span class="badge text-bg-primary">#{{ $article->category->name }}</span>
                     </div>
+                    @endif
 
+                    @auth
+                    <a href="{{ route('article.edit', compact('article')) }}" class="btn mybtn mt-3 ">Modifica</a>
+                    <form action="{{ route('article.destroy', compact('article')) }}" method="POST">
+                        @method('DELETE')
+                        @csrf
+                        <button type="submit" class="btn btn-danger  mt-3 ">Elimina</button>
+                    </form>
+                    @endauth
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 
 
